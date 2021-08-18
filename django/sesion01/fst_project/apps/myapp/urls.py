8cd97fa92ee8177030ff5deb1c03fdce21fda7f9
ccarrascod <ccarrascod@manager.cl>
2021-08-18 02:25:48 -0400

    Se agraga fst_project

---
+++ django/sesion01/fst_project/apps/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('home_usuario', views.home_usuario),
+    path('hola_usuario/<str:username>', views.hello_user),
+    path('repeticiones/<str:username>/<int:repeticiones>', views.repetir)
+]
