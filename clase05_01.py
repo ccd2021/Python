f8be7942fed3fc1e8c0dbef77f09d63f061964be
ccarrascod <ccarrascod@manager.cl>
2021-09-13 12:21:08 -0300

    Se agrega tare clase05_01.py ref: metodos de encadenamientos...

--- clase05_01.py
+++ clase05_01.py
@@ -12,6 +12,32 @@ print(carlos.name)   # salida: Michael
 print(alberto.name)  # salida: Michael
 
 
+class User2:
+    def __init__(self, username, email_address):    # ahora nuestro método tiene 2 parametros!
+        self.name = username  # y usamos los valores pasados para establecer el atributo de nombre
+        self.email = email_address		            # y el atributo email
+        # el saldo de la cuenta se establece en $ 0, así que no es necesario un tercer parámetro
+        self.account_balance = 0
+
+    # agrega el método deposit
+
+    def make_deposit(self, amount):  # toma un argumento que es el monto del depósito
+        # la cuenta del usuario específico aumenta por la cantidad del valor recibido
+        self.account_balance += amount
+        return self
+
+    # resta el método deposit
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        return self
+
+    # Muestra saldo
+    def display_user_balance(self, amount):
+        self.account_balance
+        print(self.name, ' Saldo cuenta es:', self.account_balance)
+        return self
+
+
 class User:
     def __init__(self, username, email_address):    # ahora nuestro método tiene 2 parametros!
         self.name = username  # y usamos los valores pasados para establecer el atributo de nombre
@@ -57,20 +83,34 @@ print(usr1.name, ", ", usr2.name, ", ", usr3.name)
 usr1.make_deposit(10000)
 usr1.make_deposit(20000)
 usr1.make_deposit(30000)
-usr1.make_withdrawal(30000)
-balanceUsr1 = usr1.account_balance
-print('El Saldo del cliente [', usr1.name, '] es ...$', balanceUsr1)
+usr1.make_withdrawal(50000)
+print('El Saldo del cliente [', usr1.name, '] es ...$', usr1.account_balance)
 
 usr2.make_deposit(25000)
 usr2.make_deposit(25000)
 usr2.make_withdrawal(48000)
 usr2.make_withdrawal(1)
-balanceUsr2 = usr2.account_balance
-print('El Saldo del cliente [', usr2.name, '] es ...$', balanceUsr2)
+print('El Saldo del cliente [', usr2.name, '] es ...$', usr2.account_balance)
 
 usr3.make_deposit(25000)
 usr3.make_withdrawal(5000)
 usr3.make_withdrawal(5000)
 usr3.make_withdrawal(3000)
-balanceUsr3 = usr3.account_balance
-print('El Saldo del cliente [', usr3.name, '] es ...$', balanceUsr3)
+print('El Saldo del cliente [', usr3.name, '] es ...$', usr3.account_balance)
+
+
+print("")
+print("metodos de encadenamientos")
+
+usr4 = User2("Carlos Carrasco", "aaa@ccd.cl")
+usr5 = User2("Alberto Guzman", "bbb@ccd.cl")
+usr6 = User2("Pamela Fuentes", "ccc@ccd.cl")
+
+print(usr1.name, ", ", usr2.name, ", ", usr3.name)
+
+usr4.make_deposit(10000).make_deposit(20000).make_deposit(
+    30000).make_withdrawal(50000).display_user_balance(0)
+usr5.make_deposit(25000).make_deposit(25000).make_withdrawal(
+    48000).make_withdrawal(1).display_user_balance(0)
+usr6.make_deposit(25000).make_withdrawal(5000).make_withdrawal(
+    5000).make_withdrawal(3000).display_user_balance(0)
