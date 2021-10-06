37915d083fb99f2d51481bbffbf1aa468767c3bb
ccarrascod <ccarrascod@manager.cl>
2021-10-05 23:41:00 -0300

    Actualizando al 05-10-2021

--- django/CCD/ORM/MovieCatalog/apps/movies/urls.py
+++ django/CCD/ORM/MovieCatalog/apps/movies/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('register_user', views.register_user),
     path('home', views.home),
+    path('register_movie', views.register_movie),
+    path('movies', views.movies),
+    path('director', views.director),
+    path('director_movies/<int:director_id>', views.director_movies),
 ]
